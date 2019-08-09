0b1401a295a059106585f79758d4445ffd5d7d70
swadhi <cswadhikar@gmail.com>
2019-08-09 12:21:21 +0530

    html

--- html_presentation/python_training/test.py
+++ html_presentation/python_training/test.py
@@ -2,23 +2,27 @@ import re
 
 code_file = 'test.py'
 dunder = '__'
+statements = 'for if else try except finally with print return'.split()
 
 def is_special_method(word):
-    if bool(re.search(r'__\w+__', word)):
-        return re.sub('(__\w+__)\(', tagify(word), word)
-    return word
+    return
 
-def tagify(word, class_name, tag='span'):
-    return '<{tag} class="{class_name}">{word}</{tag}>'.format(tag=tag, class_name=class_name, word=word)
-
+def tagify(string, class_name, pattern=None, tag='span'):
+    if pattern is not None:
+        return re.sub(pattern, r'<span class="{}">\1</span>'.format(class_name), string)
+    return re.sub(string, r'<span class="{}">{}</span>'.format(class_name, string), string)
 
 def process_line(line):
-    result_line = line
+    result_line = ''
     for word in line.split():
-        if is_special_method(word):
-            tagified = tagify(word, class_name='special-method')
-            print('tagified:', tagified)
-            result_line.replace(word, tagify(word, class_name='special-method'))
+
+        if bool(re.search(r'__\w+__', word)):   # special methods
+            word = tagify(word, pattern=r'(__\w+__)', class_name='special-method')
+
+        elif word in statements:  # statements
+            word = tagify(word, class_name='statement')
+
+        result_line += word + ' '
 
     return result_line
 
@@ -30,13 +34,20 @@ class FileReader:
         result_html = ''
         with open(self.filename) as f:
             for line in f:
-                result_html += process_line(line)
+                result_html += process_line(line) + '\n'
         return result_html
+
+
+# pl = process_line('def __init__():')
+# print(pl)
+#
+# pl = process_line('for i in range():')
+# print(pl)
+
 
 
 # r = is_special_method('__init__')
 # print(r)
-
 f = FileReader('/home/swadhi/python_and_selenium/html_presentation/python_training/test.py')
 r = f()
-# print(r, end='')
+print(r, end='')
---
+++ html_presentation/result.py
@@ -0,0 +1,6 @@
+class Mammal:
+    def __init__(self, name):
+        self.name = name
+
+    def __call__(self):
+        pass
