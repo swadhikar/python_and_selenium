2788719fb4f9e682278142ce20389d85c4555b19
swadhi <cswadhikar@gmail.com>
2019-08-09 14:14:51 +0530

    html

--- html_presentation/python_training/test.py
+++ html_presentation/python_training/test.py
@@ -2,27 +2,38 @@ import re
 
 code_file = 'test.py'
 dunder = '__'
-statements = 'for if else try except finally with print return'.split()
+keywords = ['print', 'False', 'class', 'finally', 'is', 'return', 'None', 'continue', 'for', 'lambda', 'try', 'True', 'def', 'from', 'nonlocal', 'while', 'and', 'del', 'global', 'not', 'with', 'as', 'elif', 'if', 'or', 'yield', 'assert', 'else', 'import', 'pass', 'break', 'except', 'in', 'raise']
+_self = 'self'
+special_chars = ['(', ')', ':', '.', '=']
 
 def is_special_method(word):
     return
 
-def tagify(string, class_name, pattern=None, tag='span'):
-    if pattern is not None:
-        return re.sub(pattern, r'<span class="{}">\1</span>'.format(class_name), string)
+def tagify(string, class_name, tag='span'):
     return re.sub(string, r'<span class="{}">{}</span>'.format(class_name, string), string)
 
 def process_line(line):
     result_line = ''
-    for word in line.split():
+
+    line_words = re.findall(r'\w+|[a-zA-Z]+|[#():,.= +\'\"]|__\w+__', line)
+
+    for index in range(len(line_words)):
+        word = line_words[index]
 
         if bool(re.search(r'__\w+__', word)):   # special methods
-            word = tagify(word, pattern=r'(__\w+__)', class_name='special-method')
+            class_name = 'special-method'
+            word = re.sub(r'__(\w+)__', r'<span class="{}">__\1__</span>'.format(class_name), word)
 
-        elif word in statements:  # statements
-            word = tagify(word, class_name='statement')
+        elif index < len(line_words) - 1 and line_words[index + 1] == '(':
+            word = tagify(word, class_name='function-call')
 
-        result_line += word + ' '
+        elif word in keywords:
+            word = tagify(word, class_name='keywords')
+
+        elif word == _self:
+            word = tagify(word, class_name='self')
+
+        result_line += word
 
     return result_line
 
@@ -38,16 +49,6 @@ class FileReader:
         return result_html
 
 
-# pl = process_line('def __init__():')
-# print(pl)
-#
-# pl = process_line('for i in range():')
-# print(pl)
-
-
-
-# r = is_special_method('__init__')
-# print(r)
-f = FileReader('/home/swadhi/python_and_selenium/html_presentation/python_training/test.py')
+f = FileReader('/home/swadhi/python_and_selenium/html_presentation/result.py')
 r = f()
 print(r, end='')
--- html_presentation/result.py
+++ html_presentation/result.py
@@ -1,6 +1,26 @@
-class Mammal:
-    def __init__(self, name):
-        self.name = name
+from contextlib import contextmanager
+from datetime import datetime as dt
+import sqlite3
 
-    def __call__(self):
-        pass
+
+class DBOperation:
+    insert_query = 'insert into department \
+                    values (4, "Marketing", 432532);'
+
+    def open_sqlite3_db(dbname='test.db'):
+        conn = None
+        try:
+            conn = sqlite3.connect(dbname)
+            yield conn.cursor()
+        except Exception as e:
+            print("Error occcured during DB operations! Caused by:\n",e)
+        finally:
+            # conn.commit() # TODO: Uncomment this line
+            conn.close()    # Resource cleanup
+            print("Db connection has been closed!")
+
+
+if __name__ == '__main__':
+    with open_sqlite3_db() as db_cursor:
+        db_cursor.execute(insert_query)
+        print('Data inserted successfully!')
